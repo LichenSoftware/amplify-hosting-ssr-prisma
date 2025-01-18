1c69dacad322e421ff96ac16d4398387a37bb6e6
jwaters <lichensoftware@gmail.com>
2025-01-17 20:41:20 -0500

    update env sh

--- update-env.sh
+++ update-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # `/amplify/prismassr/db` is just an example, switch the path used when the parameter was created
-echo "DATABASE_URL=$(aws ssm get-parameter --name '/amplify/prismassr/db' --with-decryption | jq '.Parameter.Value')" >> .env
+# echo "DATABASE_URL=$(aws ssm get-parameter --name '/amplify/prismassr/db' --with-decryption | jq '.Parameter.Value')" >> .env
+echo "DATABASE_URL=$DATABASE_URL" >> .env
